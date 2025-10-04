2ae6769429e3b8a50323e4ecfc2e994a7d841c0b
Anselm Brehme <development.anselm@breh.me>
2025-10-04 16:36:27 +0200

    fetch games for LD Event

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import {getBackendUrlFor} from "@/util";
 
-export type Game = {
+export interface Game {
     id: number,
     name: string,
     cover: string,
@@ -8,13 +8,24 @@ export type Game = {
     cool: number
 }
 
-export async function findGame(username: string): Promise<Game[]> {
+export interface JamGames {
+    'can-grade': boolean
+    games: Game[]
+}
+
+export async function findUserGames(jam :string, username: string): Promise<Game[]> {
     if (username === "???") {
         return Promise.resolve([])
     }
-    let url = getBackendUrlFor(`/ld58/userGame`) + `?username=${username}`
-    return  fetch(url)
+    let url = getBackendUrlFor(`/ld58/userGame`) + `?username=${username}&jam=${jam}`
+    return fetch(url)
         .then(r => r.json())
         .then(r => r as Game[])
 }
 
+export async function findGames(jam :string) {
+    let url = getBackendUrlFor(`/ld58/games`) + `?jam=${jam}`
+    return fetch(url)
+        .then(r => r.json())
+        .then(r => r as JamGames)
+}
--- src/ui.ts
+++ src/ui.ts
@@ -1,6 +1,7 @@
-import {findGame} from "@/server.ts";
+import {findGames, findUserGames} from "@/server.ts";
 
 const LOCALSTORAGE_USER = "user";
+const JAM_ID = "56";
 
 let userNameDlg = document.querySelector<HTMLDialogElement>("#dlg-username")!;
 let playerNamePlate = document.querySelector("#player")!;
@@ -8,10 +9,11 @@ let btnChangeUser = document.querySelector("#btn-change-user")!;
 
 btnChangeUser.addEventListener("click", openUsernameDialog)
 
-async function updatePlate(user: string) {
+
+async function updateNamePlate(user: string) {
     playerNamePlate.querySelector(".player-name")!.textContent = user;
 
-    let game = await findGame(user)
+    let game = await findUserGames(JAM_ID, user)
     if (game.length === 0) {
         // TODO fun fact?
         // # of game submissions
@@ -21,19 +23,21 @@ async function updatePlate(user: string) {
     } else {
         const randomIndex = Math.floor(Math.random() * game.length);
         const randomGame = game[randomIndex];
-        alert(`Does ${randomGame.name} ring a bell? Good.`)
+        console.log(`Does ${randomGame.name} ring a bell? Good.`, randomGame)
     }
-
-
 }
 
-export function loadUI() {
+export async function loadUI() {
     let user = localStorage.getItem(LOCALSTORAGE_USER);
     if (user === null || user === "") {
         openUsernameDialog()
     } else {
-        updatePlate(user);
+        updateNamePlate(user);
     }
+
+    let games = await findGames(JAM_ID);
+    console.log(games.games.length, "games found for LD", JAM_ID)
+    console.log("grading enabled:", games["can-grade"])
 
 }
 
@@ -50,10 +54,10 @@ function saveUserName(e: SubmitEvent) {
         const input = form.querySelector("input") as HTMLInputElement;
         const userName = input.value;
         localStorage.setItem(LOCALSTORAGE_USER, userName)
-        updatePlate(userName)
+        updateNamePlate(userName)
     } else {
         localStorage.removeItem(LOCALSTORAGE_USER);
-        updatePlate("???")
+        updateNamePlate("???")
     }
     userNameDlg.close()
 }
