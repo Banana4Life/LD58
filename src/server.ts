40356dad7d1f1af0ac7e51f64bbab123f22f25c6
Anselm Brehme <development.anselm@breh.me>
2025-10-04 20:44:37 +0200

    find current game with user games

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,16 @@ import {getBackendUrlFor} from "./util";
 
 export interface Game {
     id: number,
+    jamId: number,
     name: string,
     cover: string,
     web: string,
     cool: number
+}
+
+export interface UserGames {
+    current: Game | null
+    games: Game[]
 }
 
 export interface JamGames {
@@ -13,18 +19,20 @@ export interface JamGames {
     games: Game[]
 }
 
-export async function findUserGames(jam :string, username: string): Promise<Game[]> {
+export async function findUserGames(jam :string, username: string): Promise<UserGames> {
     if (username === "???") {
-        return Promise.resolve([])
+        return Promise.resolve({current: null, games: []})
     }
     let url = getBackendUrlFor(`/ld58/userGame`) + `?username=${username}&jam=${jam}`
+    console.log("GET", url)
     return fetch(url)
         .then(r => r.json())
-        .then(r => r as Game[])
+        .then(r => r as UserGames)
 }
 
 export async function findGames(jam :string) {
     let url = getBackendUrlFor(`/ld58/games`) + `?jam=${jam}`
+    console.log("GET", url)
     return fetch(url)
         .then(r => r.json())
         .then(r => r as JamGames)
--- src/ui.ts
+++ src/ui.ts
@@ -1,9 +1,24 @@
-import {findGames, findUserGames} from "./server.ts";
+import {findGames, findUserGames, Game} from "./server.ts";
 
 const LOCALSTORAGE_USER = "user";
 const JAM_ID = "56";
 
-let userNameDlg = document.querySelector<HTMLDialogElement>("#dlg-username")!;
+let dlgUserName = document.querySelector<HTMLDialogElement>("#dlg-username")!;
+dlgUserName.querySelector("form")?.addEventListener("submit", saveUserName)
+
+let dlgOk = document.querySelector<HTMLDialogElement>("#dlg-ok")!;
+let dlgOkCb: (() => void) | null = null;
+dlgOk.querySelector(".dlg-btn-ok")?.addEventListener("click", () => {
+    dlgOk.close();
+    // console.log(dlgOk, "close ok", dlgOkCb)
+    if (dlgOkCb) {
+        let cb = dlgOkCb
+        dlgOkCb = null
+        cb()
+    }
+})
+
+
 let playerNamePlate = document.querySelector("#player")!;
 let btnChangeUser = document.querySelector("#btn-change-user")!;
 
@@ -14,17 +29,35 @@ async function updateNamePlate(user: string) {
     playerNamePlate.querySelector(".player-name")!.textContent = user;
 
     let game = await findUserGames(JAM_ID, user)
-    if (game.length === 0) {
+    if (game.games.length === 0) {
+        let cnt = 420;
+        openOkDialog(`Did you know there are exactly ${cnt} games submitted for LD${JAM_ID}?`, () => {})
         // TODO fun fact?
         // # of game submissions
         // # of compo games
         // etc
 
     } else {
-        const randomIndex = Math.floor(Math.random() * game.length);
-        const randomGame = game[randomIndex];
-        console.log(`Does ${randomGame.name} ring a bell? Good.`, randomGame)
+        const randomIndex = Math.floor(Math.random() * game.games.length);
+        const randomGame = game.games[randomIndex];
+        openOkDialog(`Does ${randomGame.name} ring a bell? Good.`, () => askEmbedd(game.current))
+
+        console.log(randomGame);
     }
+}
+
+function askEmbedd(current: Game | null) {
+    if (current != null) {
+        openOkDialog(`Does your current game ${current.name} support embedding?`, null)
+    }
+}
+
+
+function openOkDialog(textContent: string, cb: (() => void) | null = null) {
+    dlgOk.querySelector(".content")!.textContent = textContent;
+    dlgOkCb = cb;
+    dlgOk.show()
+    console.log("open ok",textContent)
 }
 
 export async function loadUI() {
@@ -42,15 +75,14 @@ export async function loadUI() {
 }
 
 function openUsernameDialog() {
-    userNameDlg.showModal()
-    userNameDlg.querySelector("form")?.addEventListener("submit", saveUserName)
+    dlgUserName.showModal()
 }
 
 function saveUserName(e: SubmitEvent) {
     e.preventDefault()
     const form = e.currentTarget as HTMLFormElement;
     const submitter = e.submitter as HTMLElement;
-    if (submitter === userNameDlg.querySelector(".dlg-btn-submit")) {
+    if (submitter === dlgUserName.querySelector(".dlg-btn-submit")) {
         const input = form.querySelector("input") as HTMLInputElement;
         const userName = input.value;
         localStorage.setItem(LOCALSTORAGE_USER, userName)
@@ -59,5 +91,5 @@ function saveUserName(e: SubmitEvent) {
         localStorage.removeItem(LOCALSTORAGE_USER);
         updateNamePlate("???")
     }
-    userNameDlg.close()
+    dlgUserName.close()
 }
