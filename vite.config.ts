2cc34095d939d894fe7136ee4b074a084912c311
Phillip Schichtel <phillip@schich.tel>
2025-10-06 15:31:35 +0200

    compress files

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import {defineConfig} from 'vite';
+import {resolve} from 'path';
 import arraybuffer from "vite-plugin-arraybuffer";
 import svgLoader from "vite-svg-loader";
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-  plugins: [arraybuffer(), svgLoader({ defaultImport: 'url' })],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
+    plugins: [arraybuffer(), svgLoader({defaultImport: 'url'}), viteCompression()],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, './src')
+        }
+    },
+    build: {
+        target: 'esnext',
+        minify: 'esbuild',
+        sourcemap: true,
+    },
+    server: {
+        port: 3000,
+        open: true
     }
-  },
-  build: {
-    target: 'esnext',
-    minify: 'esbuild',
-    sourcemap: true
-  },
-  server: {
-    port: 3000,
-    open: true
-  }
 });
